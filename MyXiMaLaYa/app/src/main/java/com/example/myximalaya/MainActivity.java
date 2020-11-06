905e42c43b61fe88c28d10957b532167b12a5e12
zjj <1030101700@qq.com>
2020-11-06 15:04:33 +0800

    集成喜马拉雅SDK， 并且获取到数据

--- MyXiMaLaYa/app/src/main/java/com/example/myximalaya/MainActivity.java
+++ MyXiMaLaYa/app/src/main/java/com/example/myximalaya/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.myximalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String Tag = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories != null) {
+                    int size = categories.size();
+                    Log.d(Tag, "categories --< " + size);
+                    for (Category category : categories) {
+                        Log.d(Tag, "category --> " + category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(Tag, "error code -- " + i + ", error msg ==> " + s);
+            }
+        });
     }
 }
